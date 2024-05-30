26164705de597c5efe3f39b710b5d4e78cd63db5
Gabriel Bosco <gabrielgbosco1406@gmail.com>
2024-05-30 17:18:20 -0300

    Início da aba de edição da foto de perfil

--- scripts/conta.js
+++ scripts/conta.js
@@ -1,9 +1,20 @@
 function abrirAbaEdicaoPerfil() {
-    document.getElementById("fundo-editar-perfil-fechar").style.backgroundColor = "#d8dfe460";
+    document.getElementById("fundo-editar-perfil-fechar").style.backgroundColor = "#000000b7";
     document.getElementById("fundo-editar-perfil-fechar").style.height = "100vh";
 }
 
 function fecharAbaEdicaoPerfil() {
     document.getElementById("fundo-editar-perfil-fechar").style.backgroundColor = "transparent";
     document.getElementById("fundo-editar-perfil-fechar").style.height = "0";
+}
+
+function abrirAbaEdicaoFoto() {
+    document.getElementById("fundo-editar-foto-de-perfil").style.backgroundColor = "#000000b7";
+    document.getElementById("fundo-editar-foto-de-perfil").style.height = "100vh";
+    document.getElementById("botao-branco").style.color = "#ffffff"
+}
+
+function fecharAbaEdicaoFoto() {
+    document.getElementById("fundo-editar-foto-de-perfil").style.backgroundColor = "transparent";
+    document.getElementById("fundo-editar-foto-de-perfil").style.height = "0";
 }
