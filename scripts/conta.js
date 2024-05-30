7bdd5bf1e6ad9ff33ee025f46b7440b81c3cc38e
Gabriel Bosco <gabrielgbosco1406@gmail.com>
2024-05-29 23:50:46 -0300

    Adição da aba de edição de perfil na conta

---
+++ scripts/conta.js
@@ -0,0 +1,7 @@
+function abrirAbaEdicaoPerfil() {
+    document.getElementById("edicao-de-perfil").style.height = "96vh";
+}
+
+function fecharAbaEdicaoPerfil() {
+    document.getElementById("edicao-de-perfil").style.height = "0";
+}
